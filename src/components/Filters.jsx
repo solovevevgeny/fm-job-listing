b99be1cbdb1264aa5f1359ff21a88ece7ada130f
Solovev Evgeny <evgeny.solovev@gmail.com>
2024-02-28 15:53:52 +0300

    inline filters

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -3,13 +3,18 @@ import React from 'react'
 const Filters = () => {
   return (
     <div className='filters'>
-        <div className="filters__active">
+        <div className="filters__active flex-inline">
 
             <div className="filter">
               <div className="filter__name">filter</div>
               <div className="filter__icon">X</div>
             </div>
-            
+
+            <div className="filter">
+              <div className="filter__name">filter</div>
+              <div className="filter__icon">X</div>
+            </div>
+
         </div>
         <div className="filters__clear">
             <a className='green' href="">clear</a>
